6282d36c841b33a9d362988b33997523c041dc59
Straforini <134074601+PauloStraforini@users.noreply.github.com>
2025-08-08 13:29:56 -0300

    feat(routes) update new route for pets, tutors, and authentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { registerUser } from './controllers/register'
+import { authenticate } from './controllers/authenticate'
+import { petsRegister } from './controllers/pets-register'
+import { tutorRegister } from './controllers/tutors-register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerUser)
+  app.post('/sessions', authenticate)
+  app.post('/pets', petsRegister)
+  app.post('/tutors', tutorRegister)
 }
