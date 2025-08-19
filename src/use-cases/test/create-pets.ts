1e20361922589ad7d1b31bccb254fbab7bd878a4
Straforini <134074601+PauloStraforini@users.noreply.github.com>
2025-08-19 12:44:56 -0300

    refactor(controller): rename register to create

---
+++ src/use-cases/test/create-pets.ts
@@ -0,0 +1,83 @@
+import { PetAlreadyExistsError } from '../errors/pet-already-exist-error'
+import { PetsRepository } from '../../repositories/pets-repository'
+import { Pet, TYPE, SEX } from '@prisma/client'
+import { Decimal } from '@prisma/client/runtime/library'
+
+interface CreatePetUseCaseRequest {
+  name: string
+  rga: string
+  dateOfBirth: Date
+  age: string
+  sex: SEX
+  type: TYPE
+  breed: string
+  weight: Decimal
+  weightClass: string
+  primaryColor: string
+  about: string
+  microchip: string
+  dateOfApplication: Date
+  veterinarian: string
+  manufacturerOfMicrochip: string
+  socialName: string
+  origin: string
+  housing: string
+  characteristics: string
+  orgId: string
+  tutorsId?: string
+  castrated: boolean
+  nameEmergencyContact: string
+  phoneNumberEmergency: string
+  cep: string
+  street: string
+  numberHouse: string
+  complement?: string
+  neighborhood: string
+  city: string
+}
+
+interface CreatePetUseCaseResponse {
+  pet: Pet
+}
+
+export class CreatePetUseCase {
+  constructor(private petsRepository: PetsRepository) {}
+
+  async execute(
+    data: CreatePetUseCaseRequest,
+  ): Promise<CreatePetUseCaseResponse> {
+    const existingPet = await this.petsRepository.findByRga(data.rga)
+
+    if (existingPet) {
+      throw new PetAlreadyExistsError()
+    }
+
+    const pet = await this.petsRepository.create({
+      name: data.name,
+      rga: data.rga,
+      dateOfBirth: data.dateOfBirth,
+      age: data.age,
+      sex: data.sex,
+      type: data.type,
+      breed: data.breed,
+      weight: data.weight,
+      weightClass: data.weightClass,
+      primaryColor: data.primaryColor,
+      about: data.about,
+      microchip: data.microchip,
+      dateOfApplication: data.dateOfApplication,
+      veterinarian: data.veterinarian,
+      manufacturerOfMicrochip: data.manufacturerOfMicrochip,
+      socialName: data.socialName,
+      origin: data.origin,
+      housing: data.housing,
+      characteristics: data.characteristics,
+      orgId: data.orgId,
+      tutorsId: data.tutorsId,
+    })
+
+    return {
+      pet,
+    }
+  }
+}
